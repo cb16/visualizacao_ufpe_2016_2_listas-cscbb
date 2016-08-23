b455302d1c2b5ceec8aa3b8df6f1daaa560e4653
cb16 <cscbb@cin.ufpe.br>
2016-08-23 19:42:20 -0300

    first exercise first list

---
+++ lista1/problema1.js
@@ -0,0 +1,40 @@
+function getMinimum() {
+  column = document.getElementById("column").value;
+  species = document.getElementById("species").value;
+  spe = species;
+  var filteredBySpecies = iris.filter(function(value) {
+    return value.Species == species;
+  });
+  var mini = filteredBySpecies.reduce(function(pre, cur){
+    return (pre[column] < cur[column] ? pre : cur);
+  });
+  console.log("Mini: " + mini[column]);
+  d3.select("p").text(mini[column]);
+};
+
+function getAverage() {
+  var species = document.getElementById("species").value.split(" ");
+  var column = document.getElementById("column").value;
+  var filteredBySpecies = iris.filter(function(value) {
+    return species.indexOf(value.Species) != -1;
+  });
+  var keep = 0;
+  var sum = filteredBySpecies.reduce(function(pre, cur) {
+    keep = keep + cur[column];
+    return cur;
+  });
+  var average = keep / filteredBySpecies.length;
+  d3.select("p").text(average);
+}
+
+function getMaximumNotIn() {
+  var species = document.getElementById("species").value.split(" ");
+  var column = document.getElementById("column").value;
+  var filteredBySpecies = iris.filter(function(value){
+    return species.indexOf(value.Species) == -1;
+  });
+  var maxi = filteredBySpecies.reduce(function(pre, cur){
+    return (pre[column] > cur[column] ? pre : cur);
+  });
+  d3.select("p").text(maxi[column]);
+}
