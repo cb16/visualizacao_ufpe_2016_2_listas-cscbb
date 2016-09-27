11480eb74a05808b6ec0b1f638a5e04f91cb777b
cb16 <cscbb@cin.ufpe.br>
2016-09-26 21:16:12 -0300

    L3P3 axis and mouse over

--- lista3/L3P3.js
+++ lista3/L3P3.js
@@ -3,39 +3,45 @@ function timeTable() {
   var g = d3.select("#time").select("g");
   var h = d3.select("#time");
 
+  var margin = 20;
+
   var dateFormat = d3.timeFormat("%b/%y");
   var dates = ["03/2014","04/2014","05/2014","06/2014","07/2014","08/2014","09/2014","10/2014","11/2014","12/2014"];
 
-  var offset = 5;
+  var keys = [];
+
   var maxi = 0;
   var list = [];
   for(id in byType) {
     var t = [];
     var start = 0;
+    keys.push(id);
     for(it in byType[id]) {
-      t.push({x: start, y: byType[id][it] + offset});
+      t.push({x: start, y: byType[id][it] + margin});
       start += 50;
       maxi = Math.max(maxi, byType[id][it])
     }
     list.push(t);
   }
+  console.log(byType);
 
-  var scaleY = d3.scaleLinear().domain([5, maxi]).range([0, 450]);
-  var scaleX = d3.scaleLinear().domain([dateFormat("03/2014"),dateFormat("12/2014")]).range([0,490]);
+  var scaleY = d3.scaleLinear().domain([0, maxi]).range([0, 400]);
+  var scaleYReverted = d3.scaleLinear().domain([0, maxi]).range([400, 0]);
+  var scaleX = d3.scaleLinear().domain([0,10]).range([0,400]);
 
-  var xAxis = d3.axisLeft(scaleY);
-  g.append("g")
-  .attr("class", "y axis")
+  var xAxis = d3.axisLeft(scaleYReverted);
+  h.append("g")
+  .attr("transform", "translate(20, 30)")
   .call(xAxis);
 
   var yAxis = d3.axisBottom(scaleX);
-  g.append("g")
-  .attr("class", "x axis")
+  h.append("g")
+  .attr("transform", "translate(20,435)")
   .call(yAxis);
 
   var lineFunction = d3.line()
-  .x(function(d) { return d.x; })
-  .y(function(d) { return scaleY(d.y); });
+  .x(function(d) { return d.x + margin; })
+  .y(function(d) { return scaleY(d.y) + margin; });
 
   g.selectAll("path").data(list).enter()
   .append("path")
@@ -43,6 +49,16 @@ function timeTable() {
   .attr("d", function(d) { return lineFunction(d); })
   .attr("stroke", function(d,i) { return scaleColor2(i); })
   .attr("stroke-width", 2)
-  .attr("fill", "none");
+  .attr("fill", "none")
+  .on("mouseover", function(d,i) {
+    var position = d3.mouse(this);
+    h.append("text")
+    .attr("x", position[0])
+    .attr("y", 500 - position[1])
+    .text(keys[i]);
+  })
+  .on("mouseout", function() {
+    h.selectAll("text").remove();
+  });
 
 }
