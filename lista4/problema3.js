153f908e390af43d6d753c4b9c815a5168913c36
cb16 <cscbb@cin.ufpe.br>
2016-10-13 14:55:12 -0300

    L4P3 - mouseover

--- lista4/problema3.js
+++ lista4/problema3.js
@@ -1,8 +1,8 @@
 var data = [];
 var allData = [];
 var parties = [];
-var width = 600;
-var height = 600;
+var width = 2000;
+var height = 2000;
 
 var stratify = d3.stratify().id(function(d) { return d.Candidato; }).parentId(function(d) { return d.Partido; });
 var treemap = d3.treemap().size([width, height]).padding(1).round(true);
@@ -10,16 +10,19 @@ var treemap = d3.treemap().size([width, height]).padding(1).round(true);
 function getParties() {
   for(entry in data) {
     var party = data[entry].Partido;
-    if(!(party in parties)) {
+    if(parties.indexOf(party) == -1) {
       parties.push(party);
     }
   }
+  parties.pop();
+  parSize = parties.length
 }
 
-var partyColors = d3.scaleLinear().domain(parties).range(["yellow", "red"]);
+var parSize = 0;
 
 var color = function(p) {
-  return partyColors(p);
+  var scaleColor = d3.scaleLinear().domain([0,parSize]).range(["blue","red"]);
+  return scaleColor(parties.indexOf(p));
 }
 
 function organizeData() {
@@ -40,36 +43,70 @@ function organizeData() {
 }
 
 function makeTreemap() {
-  d3.csv("cand_sample.csv", function(error, d) {
+  //d3.csv("cand_sample.csv", function(error, d) {
+  d3.csv("cand_ver_recife_2016.csv", function(error, d) {
     if(error) throw error;
     data = d;
     organizeData();
 
     var root = stratify(allData)
-    .sum(function(d) { return d.Votos ? 1 : 0 })
-    .sort(function(a,b) { return b.height - a.height || b.value - a.value; });
+    .sum(function(d) { return d.Votos; });
     //console.log("root");
     //console.log(root);
 
     treemap(root);
 
-    d3.select("body")
-    .selectAll(".node")
+    d3.select("#g")
+    .selectAll(".partido")
     .data(root.leaves())
-    .enter().append("div")
-      .attr("class", "node")
-      .attr("title", function(d) { return d.id; })
-      .style("left", function(d) { return d.x0 + "px"; })
-      .style("top", function(d) { return d.y0 + "px"; })
-      .style("width", function(d) { return d.x1 - d.x0 + "px"; })
-      .style("height", function(d) { return d.y1 - d.y0 + "px"; })
-      .style("background", function(d) { while (d.depth > 1) d = d.parent; return color(d.parent); })
-    .append("div")
-      .attr("class", "node-label")
-      .text(function(d) { return d.id; });
+    .enter()
+    .append("rect")
+    .attr("class", "partido")
+    .attr("title", function(d) {
+      return d.id;
+    })
+    .attr("x", function(d) {
+      return d.x0;
+    })
+    .attr("y", function(d) {
+      return d.y0;
+    })
+    .attr("width", function(d) {
+      return d.x1 - d.x0;
+    })
+    .attr("height", function(d) {
+      return d.y1 - d.y0;
+    })
+    .attr("fill", function(d) {
+      var realColor;
+      while (d.depth > 1) {
+        if(parties.indexOf(d.parent.id) != -1) {
+          realColor = color(d.parent.id);
+        }
+        d = d.parent;
+      }
+      return realColor;
+    })
+    .on('mouseover', function(d) {
+      d3.select("#g")
+      .append("text")
+      .attr("class", "names")
+      .attr("x", function() {
+        return d.x0 + 5;
+      })
+      .attr("y", function() {
+        return d.y0 + 15;
+      })
+      .attr("fill", "white")
+      .text(function() {
+        return d.id + " - " + d.parent.id;
+      })
+    })
+    .on('mouseout', function() {
+      d3.selectAll(".names").remove();
+    });
 
   });
-  //data = d3.csvParse()
 }
 
 function init() {
